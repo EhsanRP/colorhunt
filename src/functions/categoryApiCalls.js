fbc9607a385012cdfd9dd9dcf93d2f8d88bebb94
ehsan <sir.ehrp@gmail.com>
2023-08-18 16:46:38 +0330

    finalized

--- src/functions/categoryApiCalls.js
+++ src/functions/categoryApiCalls.js
@@ -4,7 +4,7 @@ const BASE_URL = "https://colorhunt-equadesign.fandogh.cloud/api/v1/categories"
 const LOCAL_BASE_URL = "http://127.0.0.1:8080/api/v1/categories"
 
 export const fetchCategories = async () => {
-    const uri = `${BASE_URL}/all`
+    const uri = `${LOCAL_BASE_URL}/all`
     const response = await axios.get(uri, {params: {approval: true}})
     return response.data
 }
--- src/functions/paletteApiCalls.js
+++ src/functions/paletteApiCalls.js
@@ -5,38 +5,38 @@ const LOCAL_BASE_URL = "http://127.0.0.1:8080/api/v1/palettes"
 const PAGE_SIZE = 20;
 
 export const fetchPalettes = async (page) => {
-    const uri = `${BASE_URL}/all`
+    const uri = `${LOCAL_BASE_URL}/all`
     const response = await axios.get(uri, {params: {size: PAGE_SIZE, page: page},})
     return response.data
 }
 
 export const fetchAllPalettesById = async (idList) => {
-    const baseURI = `${BASE_URL}/id`
+    const baseURI = `${LOCAL_BASE_URL}/id`
     const ListURI = idList.map(id => `${baseURI}/${id}`)
     const response = await axios.all(ListURI.map(uri => axios.get(uri)))
     return response.map(res => res.data)
 }
 
 export const fetchPaletteById = async (id) => {
-    const uri = `${BASE_URL}/id/${id}`
+    const uri = `${LOCAL_BASE_URL}/id/${id}`
     const response = await axios.get(uri)
     return response.data
 }
 
 export const fetchPopular = async (page) => {
-    const uri = `${BASE_URL}/popular`
+    const uri = `${LOCAL_BASE_URL}/popular`
     const response = await axios.get(uri, {params: {size: PAGE_SIZE, page: page}})
     return response.data
 }
 
 export const fetchRandom = async (page) => {
-    const uri = `${BASE_URL}/random`
+    const uri = `${LOCAL_BASE_URL}/random`
     const response = await axios.get(uri, {params: {size: PAGE_SIZE, page: page}})
     return response.data
 }
 
 export const fetchPalettesByCategoryId = async (categoryId, page) => {
-    const uri = `${BASE_URL}/all/category/${categoryId}`
+    const uri = `${LOCAL_BASE_URL}/all/category/${categoryId}`
     const response = await axios.get(uri, {params: {size: PAGE_SIZE, page: page}})
     return response.data
 }
@@ -53,13 +53,13 @@ export const fetchPalettesByCategoryId = async (categoryId, page) => {
 // }
 //
 export const likePalette = async (id) => {
-    const uri = `${BASE_URL}/like/${id}`
+    const uri = `${LOCAL_BASE_URL}/like/${id}`
     return await axios.put(uri)
 
 }
 
 export const dislikePalette = async (id) => {
-    const uri = `${BASE_URL}/dislike/${id}`
+    const uri = `${LOCAL_BASE_URL}/dislike/${id}`
     const result = await axios.put(uri)
     return result
 }
